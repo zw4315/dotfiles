4f88d8167b89387137b7b15b0c94dbfa28be1a9e
Edward <ed.z.wu@outlook.com>
2026-06-12 01:08:49 +0800

    fix(gtags): switch GTAGSLABEL from native-pygments to universal-ctags
    native-pygments requires /usr/bin/ctags-exuberant which is often missing.
    universal-ctags handles C/C++/Python/Go/etc. out of the box.

--- home/profile.d/gtags.sh
+++ home/profile.d/gtags.sh
@@ -1,6 +1,8 @@
 # gtags (GNU Global) configuration
 # Loaded by ~/.profile via profile.d mechanism
 
-# Use Python pygments for better language support
-export GTAGSLABEL='native-pygments'
+# Use Universal Ctags plug-in parser for broader language support
+# (native-pygments requires /usr/bin/ctags-exuberant which is often missing;
+# universal-ctags handles C/C++/Python/Go/etc. out of the box.)
+export GTAGSLABEL='universal-ctags'
 export GTAGSCONF="$HOME/.global/gtags.conf"
